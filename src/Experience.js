7fe4b7295137d4cb1bd486794a66da7b311a284b
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-09-13 11:50:40 -0400

    add project page

--- src/Experience.js
+++ src/Experience.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material';
 
 export default function Experience() {
   return (
-    <div>Experience</div>
+    <>
+    <Box
+      sx={{
+
+        backgroundColor: 'primary.dark',
+        margin: "auto",
+        width: '80%',
+        height: '80%',
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        position: 'relative'
+      }}
+    > 
+      <Typography 
+      variant="h3" 
+      gutterBottom
+      sx={{
+        left: '50%'
+        
+      }}
+      >
+        Sam Giorgievski
+      </Typography>
+
+
+    </Box>
+    </>
   )
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,134 @@
-import React from 'react'
+import React from 'react';
+import { Container, Typography, Paper, Grid, CardMedia } from '@mui/material';
 
-export default function Projects() {
+const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <Container sx={{
+      marginTop: "40px"
+
+    }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper 
+          elevation={3} 
+          sx={{
+            padding: 3
+          }}>
+            <Typography variant="h4">EatSafely</Typography>
+            <Typography variant="body1">
+              EatSafely is a food safety application that allows users to check
+              the safety rating of restaurants in their area.
+            </Typography>
+            <Grid container spacing={1}>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="EatSafely Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="EatSafely Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="EatSafely Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paper 
+          elevation={3} 
+          sx={{
+            padding: 3
+          }}>
+            <Typography variant="h4">Profile Pic Generator</Typography>
+            <Typography variant="body1">
+              Profile Pic Generator is a fun tool that generates unique profile
+              pictures based on user prompts.
+            </Typography>
+            <Grid container spacing={1}>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="Profile Pic Generator Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="Profile Pic Generator Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="Profile Pic Generator Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paper elevation={3} 
+          sx={{
+            padding: 3
+          }}>
+            <Typography variant="h4">Basic API</Typography>
+            <Typography variant="body1">
+              Basic API is a simple API backend project that demonstrates CRUD
+              operations with a database.
+            </Typography>
+            <Grid container spacing={1}>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="Basic API Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="Basic API Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="Basic API Screenshot"
+                  height="200"
+                  image="https://via.placeholder.com/300x200"
+                />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Projects;
